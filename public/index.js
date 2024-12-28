dca6db548c9190fe39f8ad1f8fed80f6a554c34f
Madhu-kushwah-16 <kushwahmadhu5000@gmail.com>
2024-12-28 15:36:48 +0530

    webdevelopment file

---
+++ public/index.js
@@ -0,0 +1,11 @@
+//console.log("hii");
+//setTimeout(()=>{
+  //  console.log("hii buddy")
+//},500);
+//console.log("namaste");
+
+
+setInterval(()=>{
+    console.log("bahut dukh !!!")
+},2000);
+console.log("hii");
